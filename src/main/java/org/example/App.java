d541b8cd30f411101ceee14ae3694d5acc4d1336
gestina <gestina@inbox.ru>
2023-02-15 12:44:56 +0600

    2nd change

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,47 +1,50 @@
 package org.example;
 
-import java.io.BufferedReader;
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.fluent.Content;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-//
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 public class App {
 
 
-    public static void main(String[] args) throws IOException {
-        String query = "http://localhost:9200/";
-
-        HttpURLConnection connection = null;
-
-        try {
-            connection = (HttpURLConnection) new URL(query).openConnection();
-            connection.setRequestMethod("GET");
-            connection.setUseCaches(false);
-            connection.connect();
-
-            StringBuilder sb = new StringBuilder();
-
-            if (HttpURLConnection.HTTP_OK == connection.getResponseCode()) {
-                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                String line;
-                while ((line = in.readLine()) != null){
-                    sb.append(line);
-                    sb.append("\n");
-                }
-                System.out.println(sb.toString());
-            } else {
-                System.out.println("error: " + connection.getResponseCode() + ", " + connection.getResponseMessage());
-            }
-        } catch (Throwable cause) {
-            cause.printStackTrace();
-        } finally {
-            if (connection != null){
-                connection.disconnect();
-            }
-        }
+    public static void main(String[] args) throws Exception {
+        requestPut();
     }
+
+    public static void requestGet() throws Exception{
+        final Content getResult = Request.Get("http://localhost:9200/puttest/_doc/1")
+                .execute().returnContent();
+        System.out.println(getResult.asString());
+    }
+
+    public static void requestPut() throws IOException {
+
+        final Collection<NameValuePair> params = new ArrayList<>();
+        params.add(new BasicNameValuePair("title", "foo"));
+        params.add(new BasicNameValuePair("body", "bar"));
+        params.add(new BasicNameValuePair("userId", "1"));
+
+        final Content postResult = Request.Put("http://localhost:9200/puttest/_doc/1")
+                .bodyString("{\"title\": \"foo\",\"body\":\"bar\",\"userId\": 1}", ContentType.APPLICATION_JSON)
+                .execute().returnContent();
+        System.out.println(postResult.asString());
+    }
+
 }
 
